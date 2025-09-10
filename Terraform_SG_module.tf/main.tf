e667ac49e19cd825b9a5995f0640f0733239947d
saikirangoka <gokasaikiran15@gmail.com>
2025-09-10 11:22:26 +0530

    sg_module

--- Terraform_SG_module.tf/main.tf
+++ Terraform_SG_module.tf/main.tf
@@ -1,6 +1,21 @@
 resource "aws_security_group" "mysql_sg" {
-    name = ""
-    description = ""
+    name = local.sg_name
+    description = var.sg_description
+    vpc_id = var.vpc_id
 
-    vpc_id = aws_vpc.t
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = merge(
+        var.common_tags,
+        var.sg_tags,
+        {
+            Name = local.sg_name
+        }
+    )
 }
